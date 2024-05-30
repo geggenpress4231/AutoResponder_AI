7f2febbd20d94610e16fe0dc98d90388c80609aa
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-05-29 22:24:51 -0400

    axios apis for adding Users

---
+++ src/service/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const url = 'http://localhost:8000';
+
+export const addUser = async (data) => {
+    try {
+        let response = await axios.post(`${url}/add`, data);
+        return response.data;
+    } catch (error) {
+        console.log('Error while calling addUser API ', error);
+    }
+}
